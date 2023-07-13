f7e42ed5eeada5db282bea5082a573f07eaeee9e
AliyaK05 <138696517+AliyaK05@users.noreply.github.com>
2023-07-13 23:10:24 +0400

    Illustrating Different Set Operations

---
+++ MC tast4 sets.py
@@ -0,0 +1,17 @@
+
+
+set1 = {10,20,30,40,50}
+set2 = {5,10,15,20,25}
+
+# set union
+print("Union of both the sets is",set1 | set2)
+
+# set intersection
+print("Intersection of both the sets is",set1 & set2)
+
+# set difference
+print("Difference of both the sets is",set1 - set2 ,"and", set2-set1)
+
+# set symmetric difference
+print("Symmetric difference of both the sets is",set1 ^ set2)
+    
